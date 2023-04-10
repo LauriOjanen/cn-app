8f0a706b510f6cf2d52ea7042d0dd45444909738
LauriOjanen <78724537+LauriOjanen@users.noreply.github.com>
2023-04-10 20:01:38 +0300

    Yes and no button created
    Created yes and no button with counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,21 @@ import './App.css';
 
 function App() {
   const [joke, setJoke] = useState('');
+  const [yesCount, setYesCount] = useState(0);
+  const [noCount, setNoCount] = useState(0);
 
   function handleClick() {
     fetch('https://api.chucknorris.io/jokes/random')
       .then(response => response.json())
       .then(data => setJoke(data.value));
+  }
+
+  function handleYesClick() {
+    setYesCount(yesCount + 1);
+  }
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
   }
 
   useEffect(() => {
@@ -21,10 +31,18 @@ function App() {
       </header>
       <main>
         <div className="center">
-          <p>{joke}</p>
-          <button onClick={handleClick}>New Joke</button>
+          <div className='joke'>
+            <p>{joke}</p>
+          </div>
+
+          <button className="new-joke-button" onClick={handleClick}>New Joke</button>
+
+          <div className="button-container">
+            <button className="yes-button" onClick={handleYesClick}>Yes ({yesCount})</button>
+            <button className="no-button" onClick={handleNoClick}>No ({noCount})</button>
+          </div>
+         
         </div>
-       
       </main>
     </>
   );
